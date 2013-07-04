e3d2ae0dae4477f3e1ea52666bebb16ba16b6780
Rhenan Bartels <rhenan.bartels@gmail.com>
2013-07-04 00:41:19 -0300

    Looks Good

--- impdaspy.py
+++ impdaspy.py
@@ -3,27 +3,30 @@
 
 from numpy import fromfile
 import sys
+import matplotlib.pyplot as plt
 
 """
     Open a binary file written by DAS (LabView) and save it to a text file.
-    
-"""    
 
+"""
+plt.switch_backend('qt4Agg')
+plt.ion()
 
 fid = open(sys.argv[1], 'rb')
 dim = fromfile(fid, dtype='>u4', count=2)
-temp = fromfile(fid, dtype='>f',count=dim[0] * dim[1]).reshape(dim[0],
-                                                                  dim[1], order='F').T # Change to column reshape format (Fortran)
+temp = fromfile(fid, dtype='>f',count=dim[0] * dim[1]).reshape(dim[1],
+                dim[0], order='F').T # Change to column reshape format (Fortran)
+
 nchannels = temp[0, 1]
 fs = temp[0, 0]
-temp = temp[1::, :]
-baseline = temp[0:nchannels, 0]
-temp = temp[nchannels::, :]
 dim = fromfile(fid, dtype='>u4', count=1)
-name = fromfile(fid, dtype='>u4') #tell  = 72
-name1 = fromfile(fid, dtype='B', count=name) #tell = 77 dtype=B -> uchar
-name = fromfile(fid, dtype='>u4') #tell  = 81
-name1 = fromfile(fid, dtype='B', count=name) #tell = 88 dtype=B -> uchar
-hora = fromfile(fid, dtype='>u4', count=1) #tell = 92
-hora1 = fromfile(fid, dtype='B', count=hora) #tell = 100
+Name = []
+
+for k in xrange(dim):
+    Name.append(fromfile(fid,dtype='B',count=fromfile(fid, dtype='>u4',
+                                                      count=1)))
+
+hour = fromfile(fid, dtype='B', count=fromfile(fid, dtype='>u4', count=1))
 signal = fromfile(fid, dtype='>f')
+signal = signal.reshape(dim, len(signal) / float(dim), order='F')
+plt.plot(signal[0])
